7c1a4ac2c2a7e6c3f81833b124c29871dd809289
Farid Lotfi <138003177+fariidlotfi@users.noreply.github.com>
2024-04-03 13:43:11 +0330

    fix: api link

--- src/Hooks/shortUrlHook.js
+++ src/Hooks/shortUrlHook.js
@@ -1,15 +1,11 @@
-
 export async function shortURL(requestedURL) {
-  const response = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://cleanuri.com/api/v1/shorten",
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ url: requestedURL }),
-    }
-  )
+  const response = await fetch("https://cleanuri.com/api/v1/shorten", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ url: requestedURL }),
+  })
     .then((res) => res.json())
     .catch((err) => {
       throw new Error(err);
